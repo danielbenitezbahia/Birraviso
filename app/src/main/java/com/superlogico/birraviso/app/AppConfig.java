795d93a5755ceed760f78109aed771378f4741a8
Daniel <daniel.benitez.bahia@gmail.com>
2017-02-01 19:35:41 -0300

    asd

---
+++ app/src/main/java/com/superlogico/birraviso/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.superlogico.birraviso.app;
+
+/**
+ * Created by Daniel on 1/2/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://timbaviso.esy.es/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://timbaviso.esy.es/android_login_api/register.php";
+}

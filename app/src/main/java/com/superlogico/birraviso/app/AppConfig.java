5ceefa30e1879ad7eb0d44f6726f5a1a0f395fb7
daniel.benitez <daniel.benitez.bahia@gmail.com>
2017-02-01 17:56:11 -0300

    Comit del día

--- app/src/main/java/com/superlogico/birraviso/app/AppConfig.java
+++ app/src/main/java/com/superlogico/birraviso/app/AppConfig.java
@@ -6,8 +6,8 @@ package com.superlogico.birraviso.app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "timbaviso.esy.es/android_login_api/login.php";
+    public static String URL_LOGIN = "http://timbaviso.esy.es/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "timbaviso.esy.es/android_login_api/register.php";
+    public static String URL_REGISTER = "http://timbaviso.esy.es/android_login_api/register.php";
 }

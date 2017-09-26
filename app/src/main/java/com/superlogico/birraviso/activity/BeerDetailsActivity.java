a9cbc5b70b20b16451a734d2612c383638dbdc2e
daniel.benitez <daniel.benitez.bahia@gmail.com>
2017-09-26 17:23:31 -0300

    lll

--- app/src/main/java/com/superlogico/birraviso/activity/BeerDetailsActivity.java
+++ app/src/main/java/com/superlogico/birraviso/activity/BeerDetailsActivity.java
@@ -35,6 +35,7 @@ import com.superlogico.birraviso.helper.SessionManager;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -97,6 +98,10 @@ public class BeerDetailsActivity extends AppCompatActivity {
             "Fruit Beer","Fruit and Spice Beer","Specialty Fruit Beer","Spice"," Herb"," or Vegetable Beer","Autumn Seasonal Beer","Winter Seasonal Beer",
             "Alternative Grain Beer","Alternative Sugar Beer","Classic Style Smoked Beer","Specialty Smoked Beer",
             "Wood-Aged Beer","Specialty Wood-Aged Beer","Clone Beer","Mixed-Style Beer","Experimental Beer","Dorada Pampeana","IPA Argenta"};
+
+    private  String[] srmColors = {"#FFE699","#FFD878","#FFCA5A","#FFBF42","#FBB123","#F8A600","#F39C00","#EA8F00","#E58500","#DE7C00","#D77200","#CF6900","#CB6200","#C35900","#BB5100","#B54C00","#B04500",
+            "#A63E00","#A13700","#9B3200","#952D00","#8E2900","#882300","#821E00","#7B1A00","#771900","#701400","#6A0E00","#660D00","#5E0B00","#5A0A02","#600903","#520907","#4C0505",
+            "#470606","#440607","#3F0708","#3B0607","#3A070B","#36080A"};
     private HashMap<String, String> profileDetails;
     private String beer_hb_id;
     private android.support.v7.app.ActionBar actionBar;
@@ -115,10 +120,6 @@ public class BeerDetailsActivity extends AppCompatActivity {
      /*   mDrawable = ContextCompat.getDrawable(this, R.drawable.birra);
         mDrawable.setColorFilter(new PorterDuffColorFilter(0xfff000,PorterDuff.Mode.DARKEN));*/
 
-        Resources res = this.getResources();
-        final ImageView image = (ImageView) findViewById(R.id.imageview_icon);
-        final int newColor = res.getColor(R.color.bg_login);
-        image.setColorFilter(newColor, PorterDuff.Mode.SRC_ATOP);
 
         Intent myIntent = getIntent(); // gets the previously created intent
         final String beerId = myIntent.getStringExtra(KEY_UID);
@@ -132,6 +133,14 @@ public class BeerDetailsActivity extends AppCompatActivity {
         beer_id = myIntent.getStringExtra(BEER_ID);
         DataHolder dataHolder = DataHolder.getInstance();
         favoriteMode = dataHolder.isFavoritesMode();
+
+        Resources res = this.getResources();
+        final ImageView image = (ImageView) findViewById(R.id.imageview_icon);
+        //int colorResource = this.getResId("srmColor" + beerSrm, Color.class);
+        int resourceId = this.getResources().getIdentifier("srmColor" + beerSrm, "color", this.getPackageName());
+        final int newColor = res.getColor(resourceId);
+        image.setColorFilter(newColor, PorterDuff.Mode.SRC_ATOP);
+
 
 
         tvBeerName = (TextView) findViewById(R.id.textview_title);
@@ -332,6 +341,17 @@ public class BeerDetailsActivity extends AppCompatActivity {
         // Adding request to request queue
         String a = "0";
         AppController.getInstance().addToRequestQueue(strReq,tag_string_req);
+    }
+
+    public static int getResId(String resName, Class<?> c) {
+
+        try {
+            Field idField = c.getDeclaredField(resName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
 
     private void showDialog() {
